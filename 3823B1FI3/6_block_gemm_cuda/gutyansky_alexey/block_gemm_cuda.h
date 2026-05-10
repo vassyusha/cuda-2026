161aa627569417a09f79a9b988077ae73d90a076
Alexey Gutyansky <75169888+AlexixRugis@users.noreply.github.com>
2026-05-10 22:03:33 +0300

    6_block_gemm_cuda task

---
+++ 3823B1FI3/6_block_gemm_cuda/gutyansky_alexey/block_gemm_cuda.h
@@ -0,0 +1,10 @@
+#ifndef __BLOCK_GEMM_CUDA_H
+#define __BLOCK_GEMM_CUDA_H
+
+#include <vector>
+
+std::vector<float> BlockGemmCUDA(const std::vector<float>& a,
+                                 const std::vector<float>& b,
+                                 int n);
+
+#endif // __BLOCK_GEMM_CUDA_H
